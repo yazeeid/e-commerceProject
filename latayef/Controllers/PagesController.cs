f2292a6a46f8b2d416bbe5ea1087535862f5c37e
Mo7ammad Jamal <mj266501@gmail.com>
2024-11-01 01:35:12 +0300

    Teamplate has been added

---
+++ latayef/Controllers/PagesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace latayef.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+        public IActionResult about()
+        {
+            return View();
+        }
+
+        public IActionResult blog()
+        {
+            return View();
+        }
+
+
+        public IActionResult blogSingle()
+        {
+            return View();
+        }
+
+        public IActionResult cart() {
+            return View();
+        }
+
+        public IActionResult checkout() {
+            return View();
+        }
+
+
+        public IActionResult contact() {
+
+            return View();
+        }
+
+        public IActionResult productSingle() {
+            return View();
+        }
+
+        public IActionResult shop() {
+            return View();
+        }
+        public IActionResult wishList() { return View(); }
+    }
+}
